8a98403f909c1e142f5a32094442e55f6502d7c3
dipanker <dipanker_kr@outlook.com>
2019-07-02 23:33:51 +0530

    local repo

---
+++ FunctionalProgramming/src/com/functionalprogramming/PrimeNumber.java
@@ -0,0 +1,22 @@
+package com.functionalprogramming;
+
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class PrimeNumber {
+	public static void main(String[] args) {
+		System.out.println(1 +" "+ isPrime(1));
+		System.out.println(2 +" "+ isPrime(2));
+		System.out.println(3 +" "+ isPrime(3));
+		System.out.println(4 +" "+ isPrime(4));
+		System.out.println(5 +" "+ isPrime(5));
+	}
+	
+	public static boolean isPrime(int number) {
+		IntPredicate isDivisible = divisor -> number % divisor == 0;
+		
+		return number > 1 && 
+				IntStream.range(2, number)
+						 .noneMatch(isDivisible);
+	}
+}
